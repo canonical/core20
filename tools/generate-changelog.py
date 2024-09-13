dacc3fc02fecde347a6a72c930ce4dd8b1c0db3c
Philip Meulengracht <the_meulengracht@hotmail.com>
2024-09-13 11:42:23 +0200

    many: introduce changelog for the snap repository itself prior to package changes

--- tools/generate-changelog.py
+++ tools/generate-changelog.py
@@ -22,11 +22,13 @@
 # obtained from the debian changelog for the different packages.
 
 import argparse
+from datetime import datetime
 import debian.changelog
 import debian.debian_support
 import gzip
 import os
 import requests
+import subprocess
 import sys
 import yaml
 from collections import namedtuple
@@ -37,7 +39,8 @@ from collections import namedtuple
 # keep the list short to not increase the time it takes
 # to generate changelogs
 pkg_allowed_list = [
-    'distro-info-data', # is removed during hook
+    'apt', # removed during hook
+    'debconf' # removed during hook
 ]
 
 # Returns a dictionary from package name to version, using
@@ -167,26 +170,89 @@ def compare_manifests(old_manifest_p, new_manifest_p, docs_d):
     return changes
 
 
+def find_commit_in_changelog(clog_p) -> str:
+    if clog_p == "" or not os.path.exists(clog_p):
+        print(f"No previous changelog existed at {clog_p}, skipping changelog generation for local repo")
+        return ""
+
+    # expect commit in the first line
+    with open(clog_p, "r") as f:
+        line = f.readline().strip()
+        if "commit" in line:
+            tokens = line.split("/")
+            return tokens[-1]
+        return ""
+
+
+def read_commit_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+
+def remove_suffix(input_string, suffix):
+    if suffix and input_string.endswith(suffix):
+        return input_string[:-len(suffix)]
+    return input_string
+
+
+def read_remote_git_url() -> str:
+    remote_url = subprocess.check_output(['git', 'remote', 'get-url', 'origin']).decode('ascii').strip()
+    if remote_url.startswith("git@github.com:"):
+        remote_url = remote_url.replace("git@github.com:", "https://github.com/")
+    return remove_suffix(remote_url, ".git")
+
+
+def log_between_commits(name, start, end):
+    try:
+        return subprocess.check_output(['git', 'shortlog', '--pretty=short', f'{start}..{end}']).decode()
+    except:
+        # if there is no path from start..end then this might fail, however this
+        # should only happen if the branch has diverged so much that the previous
+        # release commit does not exist in the current fork. In this case let us
+        # notify that we could not generate the changelog
+        print(f"Failed to run 'git log' for the current repo starting at commit {start}, has branch diverged to much?")
+        return f'No detected changes for the {name} snap\n\n'
+
+
 def main():
     parser = argparse.ArgumentParser(description="Manifest changelog generator")
 
-    parser.add_argument('old', metavar='old-manifest', help='Path to the manifest of the previous snap')
-    parser.add_argument('new', metavar='new-manifest', help='Path to the manifest of the new snap')
-    parser.add_argument('docs', metavar='docs-dir', help='Path to the usr/share/doc directory in the rootfs of the new snap')
-    parser.add_argument('out', help='Optionally a path to where the changelog should be written')
+    parser.add_argument('old', metavar='previous-snap-root', help='Path to the root of the previous snap directory')
+    parser.add_argument('new', metavar='new-snap-root', help='Path to the root of the new snap directory')
+    parser.add_argument('name', help='The name of the snap')
     args = parser.parse_args()
 
-    old_manifest = args.old
-    new_manifest = args.new
-    docs_dir = args.docs
+    old_changelog = os.path.join(args.old, "usr", "share", "doc", "ChangeLog")
+    new_changelog = os.path.join(args.new, "usr", "share", "doc", "ChangeLog")
+    old_manifest = os.path.join(args.old, "usr", "share", "snappy", "dpkg.yaml")
+    new_manifest = os.path.join(args.new, "usr", "share", "snappy", "dpkg.yaml")
+    docs_dir = os.path.join(args.new, "usr", "share", "doc")
 
-    changes = '[ Changes in primed packages ]\n\n'
+    # get previous commit for the base, however important to note here that
+    # the previous changelog might not exist (i.e before this was introduced)
+    # and thus this might be empty.
+    pcommit = find_commit_in_changelog(old_changelog)
+    ccommit = read_commit_hash()
+
+    # add a header that helps us audit where the current build is
+    # sourced from.
+    now = datetime.now()
+    changes = f'{now.strftime("%d/%m/%Y")}, commit {read_remote_git_url()}/tree/{ccommit}\n\n'
+    changes += f'[ Changes in the {args.name} snap ]\n\n'
+
+    # Is there a previous commit? Then we get a log between them
+    # if pcommit != ccommit.
+    if pcommit != "" and pcommit != ccommit:
+        changes += log_between_commits(args.name, pcommit, ccommit)
+    else:
+        changes += f'No detected changes for the {args.name} snap\n\n'
+
+    changes += '[ Changes in primed packages ]\n\n'
     pkg_changes = compare_manifests(old_manifest, new_manifest, docs_dir)
     if pkg_changes != '':
         changes += pkg_changes
     else:
         changes += 'No changes for primed packages\n\n'
-    with open(args.out, "w") as f:
+    with open(new_changelog, "w") as f:
         f.write(changes)
     return 0
 
